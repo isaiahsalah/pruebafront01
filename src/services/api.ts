9bc19b2e8a1b0d55ebfb8cdfc5f6c9b7a3a75a4d
Isaias <isasalas145@gmail.com>
2025-02-19 04:58:45 -0400

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,7 @@
-export const url = `http://localhost:3000/`;
-export const urlApi = `http://localhost:3000/api/`;
+//export const url = `http://localhost:3000/`;
+//export const urlApi = `http://localhost:3000/api/`;
+export const url = `http://https://pruebaback01-rwam.onrender.com/`;
+
+export const urlApi = `http://https://pruebaback01-rwam.onrender.com/api/`;
 
 export const urlUser = "user/";
